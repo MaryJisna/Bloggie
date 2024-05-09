0eaadeabb6be20f08f6af6dd52f506801c80f336
MaryJisna <maryjisna0103@gmail.com>
2024-05-09 17:27:18 +0530

    Add project files.

---
+++ Bloggie.Web/Controllers/AdminTagController.cs
@@ -0,0 +1,42 @@
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domin;
+using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminTagController : Controller
+    {
+        private readonly BloggieDBContext bloggieDBContext;
+
+        public AdminTagController(BloggieDBContext bloggieDBContext)
+        {
+            this.bloggieDBContext = bloggieDBContext;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult SubmitTag(AddTagReq addTagReq)
+        {
+
+            //var name=Request.Form["name"];
+            //var displayname = Request.Form["displayname"];       Read incoming req. one by one.  no need for this model binding more usefull.
+            
+            //*** mapping AddTagRequest to tag domain model
+            var tag = new Tag
+            {
+                Name = addTagReq.Name,
+                DisplayName = addTagReq.DisplayName
+            };
+            bloggieDBContext.Tags.Add(tag);
+            bloggieDBContext.SaveChanges();
+            //var name=addTagReq.Name;
+            //var displayname = addTagReq.DisplayName;
+            return View("Add");
+        }
+    }
+}
---
+++ Bloggie.Web/Data/BloggieDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Bloggie.Web.Models.Domin;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Data
+{
+    public class BloggieDBContext : DbContext
+    {
+        public BloggieDBContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
